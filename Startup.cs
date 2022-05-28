2f8fd17916823557043e4e68e9bd0f1b30d88970
dc morrison <nosirromd@gmail.com>
2022-05-28 23:48:29 +0100

    add SQL server DB context to DI service collection

--- Startup.cs
+++ Startup.cs
@@ -7,6 +7,7 @@ using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.HttpsPolicy;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
@@ -34,6 +35,7 @@ namespace Commander
                 c.SwaggerDoc("v1", new OpenApiInfo { Title = "Commander", Version = "v1" });
             });
             services.AddScoped<ICommanderRepo, MockCommanderRepo>();
+            services.AddDbContext<CommanderContext>(opt => opt.UseSqlServer(Configuration.GetConnectionString("CommanderConnection")));
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
